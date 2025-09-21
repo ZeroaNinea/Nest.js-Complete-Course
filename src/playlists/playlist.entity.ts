56b160fdd589a93e3d227972197aa1b1d6597ac9
Catgirl <zeroaninea@gmail.com>
2025-09-21 23:26:43 +0400

    the playlist.entity created

---
+++ src/playlists/playlist.entity.ts
@@ -0,0 +1,10 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity('playlists')
+export class Playlist {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+}
