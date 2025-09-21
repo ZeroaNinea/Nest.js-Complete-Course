3c1bf3e617811999f02ccaa1be620b424495984f
Catgirl <zeroaninea@gmail.com>
2025-09-21 18:58:30 +0400

    mock the SongsService

--- src/songs/songs.module.ts
+++ src/songs/songs.module.ts
@@ -2,14 +2,24 @@ import { Module } from '@nestjs/common';
 import { SongsController } from './songs.controller';
 import { SongsService } from './songs.service';
 
+const mockSongsService = {
+  findAll() {
+    return [{ id: 1, title: 'Lasting lover' }];
+  },
+};
+
 @Module({
   controllers: [SongsController],
   providers: [
-    SongsService,
+    // SongsService,
     // {
     //   provide: SongsService,
     //   useClass: SongsService,
     // },
+    {
+      provide: SongsService,
+      useValue: mockSongsService,
+    },
   ],
 })
 export class SongsModule {}
