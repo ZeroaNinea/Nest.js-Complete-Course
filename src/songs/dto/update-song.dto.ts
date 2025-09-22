9a1abed0676c394cdec9c1dd10a3b35ec980b79a
Catgirl <zeroaninea@gmail.com>
2025-09-22 22:12:43 +0400

    IsOptional added into the DTO

--- src/songs/dto/update-song.dto.ts
+++ src/songs/dto/update-song.dto.ts
@@ -1,23 +1,34 @@
 import { Type } from 'class-transformer';
-import { IsString, IsArray, IsDate, IsMilitaryTime } from 'class-validator';
+import {
+  IsString,
+  IsArray,
+  IsDate,
+  IsMilitaryTime,
+  IsOptional,
+} from 'class-validator';
 
 export class UpdateSongDto {
   @Type(() => Number)
   id: number;
 
   @IsString()
+  @IsOptional()
   readonly title: string;
 
   @IsArray()
+  @IsOptional()
   readonly artists: [string];
 
   @IsDate()
+  @IsOptional()
   @Type(() => Date)
   readonly releasedDate: Date;
 
   @IsMilitaryTime()
+  @IsOptional()
   readonly duration: Date;
 
   @IsString()
+  @IsOptional()
   readonly lyrics: string;
 }
