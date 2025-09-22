1234141274acf722546ea260ff0d88753c263723
Catgirl <zeroaninea@gmail.com>
2025-09-22 13:49:03 +0400

    PostgreSQL connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import {
   // RequestMethod,
 } from '@nestjs/common';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SongsModule } from './songs/songs.module';
@@ -12,12 +14,29 @@ import { LoggerMiddleware } from './common/middleware/logger/logger.middleware';
 import { SongsController } from './songs/songs.controller';
 import { DevConfigService } from './common/services/dev-config/dev-config.service';
 import { PlayListModule } from './play-list/play-list.module';
+import { Song } from './songs/song.entity';
+import { Artist } from './artists/artist.entity';
+import { User } from './users/user.entity';
+import { Playlist } from './playlists/playlist.entity';
 
 const devConfig = { port: 3000 };
 const proConfig = { port: 4000 };
 
 @Module({
-  imports: [SongsModule, PlayListModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: 'spotify-clone',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'password',
+      entities: [Song, Artist, User, Playlist],
+      synchronize: true,
+    }),
+    SongsModule,
+    PlayListModule,
+  ],
   controllers: [AppController],
   providers: [
     AppService,
