0eacd97187ff75b98ac906b3121d247a961bd965
Catgirl <zeroaninea@gmail.com>
2025-09-23 22:29:46 +0400

    TypeORM imported

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UserService } from '../user/user.service';
+import { User } from '../common/entities/user.entity';
 
 @Module({
-  providers: [AuthService],
+  imports: [TypeOrmModule.forFeature([User])],
+  providers: [AuthService, UserService],
   controllers: [AuthController],
   exports: [AuthService],
 })
