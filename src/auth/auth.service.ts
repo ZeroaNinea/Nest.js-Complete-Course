45cc210fa442c0c8d3af2120885ed5765e094b3f
Catgirl <zeroaninea@gmail.com>
2025-09-24 00:59:47 +0400

    return user data without a password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import bcrypt from 'bcryptjs';
 
 import { UserService } from '../user/user.service';
+import { LoginDto } from './dto/login.dto';
+import { PopulatedUser } from '../common/interface/populated-user.interface';
 
 @Injectable()
 export class AuthService {
   constructor(private userService: UserService) {}
+
+  async login(loginDto: LoginDto): Promise<PopulatedUser> {
+    const user = await this.userService.findOne(loginDto);
+
+    const passwordMatched = await bcrypt.compare(
+      loginDto.password,
+      user.password,
+    );
+
+    if (!passwordMatched) {
+      throw new UnauthorizedException('Password is incorrect.');
+    }
+
+    const populatedUser = {
+      id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+    };
+
+    return populatedUser;
+  }
 }
---
+++ src/common/interface/populated-user.interface.ts
@@ -0,0 +1,6 @@
+export interface PopulatedUser {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+}
