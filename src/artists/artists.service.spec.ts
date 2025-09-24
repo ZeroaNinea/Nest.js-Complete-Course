46ede0ea535c62c6aae7d8e0950f6eadca52d59b
Catgirl <zeroaninea@gmail.com>
2025-09-24 14:22:43 +0400

    the findArtist method added

--- src/artists/artists.service.spec.ts
+++ src/artists/artists.service.spec.ts
@@ -1,4 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { Artist } from '../common/entities/artist.entity';
+
 import { ArtistsService } from './artists.service';
 
 describe('ArtistsService', () => {
@@ -6,6 +10,14 @@ describe('ArtistsService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot({
+          type: 'sqlite',
+          database: ':memory:',
+          synchronize: true,
+        }),
+        TypeOrmModule.forFeature([Artist]),
+      ],
       providers: [ArtistsService],
     }).compile();
 
--- src/artists/artists.service.ts
+++ src/artists/artists.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { Artist } from '../common/entities/artist.entity';
 
 @Injectable()
-export class ArtistsService {}
+export class ArtistsService {
+  constructor(
+    @InjectRepository(Artist) private artistRepository: Repository<Artist>,
+  ) {}
+
+  async findArtist(userId: number): Promise<Artist> {
+    const artist = await this.artistRepository.findOneBy({
+      user: { id: userId },
+    });
+
+    if (!artist) {
+      throw new HttpException('Artist not found.', HttpStatus.NOT_FOUND);
+    }
+
+    return artist;
+  }
+}
