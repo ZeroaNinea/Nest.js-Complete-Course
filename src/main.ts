db85fb2a851f03c05d9e89f5d48f3ad047cb31db
Catgirl <zeroaninea@gmail.com>
2025-09-28 00:04:19 +0400

    module fixed

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,13 @@ import { ConfigService } from '@nestjs/config';
 
 import { AppModule } from './app.module';
 
+declare const module: {
+  hot?: {
+    accept: () => void;
+    dispose: (callback: () => Promise<void>) => void;
+  };
+};
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
@@ -13,6 +20,11 @@ async function bootstrap() {
   // await seedService.seed();
   const configService = app.get(ConfigService);
   await app.listen(configService.get<number>('PORT') ?? 3000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap()
   .then(() => console.log(' 🚀 Server started'))
