0ae636a4513b28f3834a145a9b6509cb91e44e6c
Catgirl <zeroaninea@gmail.com>
2025-09-27 14:19:38 +0400

    the configurations.ts file created

---
+++ src/config/configurations.ts
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { Song } from '../common/entities/song.entity';
+import { Artist } from '../common/entities/artist.entity';
+import { User } from '../common/entities/user.entity';
+import { Playlist } from '../common/entities/playlist.entity';
+
+export default () => ({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432', 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  autoLoadEntities: true,
+  synchronize: true,
+  entities: [Song, Artist, User, Playlist],
+});
