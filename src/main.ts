58ed0741b897cea7e90170240cae97b995e211da
Catgirl <zeroaninea@gmail.com>
2025-09-27 04:59:43 +0400

    cries

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 
+import { SeedService } from './seed/seed.service';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  const seedService = app.get(SeedService);
+  await seedService.seed();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap()
