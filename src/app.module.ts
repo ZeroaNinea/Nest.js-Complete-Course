3be761ed39bb11933e09285ef89c3d4122c627ca
Catgirl <zeroaninea@gmail.com>
2025-09-27 22:53:35 +0400

    validation imported

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import {
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { UuidModule } from 'nestjs-uuid';
-import Joi from 'joi';
+// import Joi from 'joi';
 
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
@@ -30,6 +30,8 @@ import { ArtistsModule } from './artists/artists.module';
 import { SeedModule } from './seed/seed.module';
 import configurations from './config/configurations';
 
+import { validate } from 'env.validation';
+
 const devConfig = { port: 3000 };
 const proConfig = { port: 4000 };
 
@@ -38,14 +40,15 @@ const proConfig = { port: 4000 };
     ConfigModule.forRoot({
       envFilePath: ['.env.development.local', '.env.production.local'],
       isGlobal: true,
-      validationSchema: Joi.object({
-        DB_HOST: Joi.string().required(),
-        DB_PORT: Joi.number().required(),
-        DB_USERNAME: Joi.string().required(),
-        DB_PASSWORD: Joi.string().required(),
-        DB_NAME: Joi.string().required(),
-      }),
+      // validationSchema: Joi.object({
+      //   DB_HOST: Joi.string().required(),
+      //   DB_PORT: Joi.number().required(),
+      //   DB_USERNAME: Joi.string().required(),
+      //   DB_PASSWORD: Joi.string().required(),
+      //   DB_NAME: Joi.string().required(),
+      // }),
       load: [configurations],
+      validate: validate,
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
