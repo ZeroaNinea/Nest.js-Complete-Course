1fe7d2b9d4b96067d99c640a73ca12a62513c51e
Catgirl <zeroaninea@gmail.com>
2025-09-29 01:53:30 +0400

    ApiProperty added into the user.entity.ts

--- src/common/entities/user.entity.ts
+++ src/common/entities/user.entity.ts
@@ -1,5 +1,6 @@
 import { Exclude } from 'class-transformer';
 import { Column, Entity, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
 
 import { Playlist } from './playlist.entity';
 
@@ -8,31 +9,63 @@ export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({
+    example: 'John',
+    description: 'User first name.',
+  })
   @Column()
   firstName: string;
 
+  @ApiProperty({
+    example: 'Doe',
+    description: 'User last name.',
+  })
   @Column()
   lastName: string;
 
+  @ApiProperty({
+    example: 'oKl1D@example.com',
+    description: 'User email.',
+  })
   @Column({ unique: true })
   email: string;
 
+  @ApiProperty({
+    example: '123456',
+    description: 'User password.',
+  })
   @Column()
   @Exclude()
   password: string;
 
+  @ApiProperty({
+    example: '123456',
+    description: 'User 2FA secret.',
+  })
   @Column({ nullable: true, type: 'text' })
   twoFASecret: string | null;
 
+  @ApiProperty({
+    example: true,
+    description: 'User 2FA status.',
+  })
   @Column({ default: false, type: 'boolean' })
   enable2FA: boolean;
 
+  @ApiProperty({
+    example: '123456',
+    description: 'User api key.',
+  })
   @Column()
   apiKey: string;
 
   // @Column()
   // phone: string;
 
+  @ApiProperty({
+    type: () => [Playlist],
+    description: 'User playlists.',
+  })
   @OneToMany(() => Playlist, (playList) => playList.user)
   playLists: Playlist[];
 }
