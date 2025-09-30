6e7cb051a8d02f0150d2c043207d8c87d63fba23
Catgirl <zeroaninea@gmail.com>
2025-09-30 21:53:35 +0400

    album.model.ts created

---
+++ src/common/schemas/album.model.ts
@@ -0,0 +1,22 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument, Types } from 'mongoose';
+
+import { Song } from './song.model';
+
+export type AlbumDocument = HydratedDocument<Album>;
+
+@Schema()
+export class Album {
+  @Prop({
+    required: true,
+  })
+  title: string;
+
+  @Prop({
+    type: [Types.ObjectId],
+    ref: 'songs',
+  })
+  songs: Song[];
+}
+
+export const AlbumSchema = SchemaFactory.createForClass(Album);
