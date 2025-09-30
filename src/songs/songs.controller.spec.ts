08b85ffab099c3cecbe92ad6b01ea157f479b3aa
Catgirl <zeroaninea@gmail.com>
2025-09-30 19:03:57 +0400

    mongodb-memory-server installed and integrated

--- src/songs/songs.controller.spec.ts
+++ src/songs/songs.controller.spec.ts
@@ -1,13 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
 
 import { SongsService } from './songs.service';
 import { SongsController } from './songs.controller';
 
+import { Song, SongSchema } from '../common/schemas/song.model';
+
 describe('SongsController', () => {
   let controller: SongsController;
+  let mongodb: MongoMemoryServer;
 
   beforeEach(async () => {
+    mongodb = await MongoMemoryServer.create();
+    const uri = mongodb.getUri();
+
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        MongooseModule.forRoot(uri),
+        MongooseModule.forFeature([{ name: Song.name, schema: SongSchema }]),
+      ],
       controllers: [SongsController],
       providers: [SongsService],
     }).compile();
--- src/songs/songs.service.spec.ts
+++ src/songs/songs.service.spec.ts
@@ -1,11 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+
+import { Song, SongSchema } from '../common/schemas/song.model';
 import { SongsService } from './songs.service';
 
 describe('SongsService', () => {
   let service: SongsService;
+  let mongodb: MongoMemoryServer;
 
   beforeEach(async () => {
+    mongodb = await MongoMemoryServer.create();
+    const uri = mongodb.getUri();
+
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        MongooseModule.forRoot(uri),
+        MongooseModule.forFeature([{ name: Song.name, schema: SongSchema }]),
+      ],
       providers: [SongsService],
     }).compile();
 
