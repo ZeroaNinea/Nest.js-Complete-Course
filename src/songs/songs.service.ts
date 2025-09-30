0022843a79ef7722c38e0687efc8372841500f46
Catgirl <zeroaninea@gmail.com>
2025-09-30 18:46:36 +0400

    the create method added

--- src/songs/songs.service.ts
+++ src/songs/songs.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import { Song, SongDocument } from '../common/schemas/song.model';
+import { CreateSongDto } from '../common/dto/create-song-dto';
 
 @Injectable()
-export class SongsService {}
+export class SongsService {
+  constructor(@InjectModel(Song.name) private songModel: Model<SongDocument>) {}
+
+  async create(createSongDto: CreateSongDto) {
+    const song = await this.songModel.create(createSongDto);
+    return song;
+  }
+}
