6866844b8bf5e5e360aeb47529a2adae450bfcfc
Catgirl <zeroaninea@gmail.com>
2025-09-30 22:50:30 +0400

    albums.service.ts created

--- src/albums/albums.service.spec.ts
+++ src/albums/albums.service.spec.ts
@@ -1,11 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AlbumsService } from './albums.service';
+import { Album, AlbumSchema } from '../common/schemas/album.model';
 
 describe('AlbumsService', () => {
   let service: AlbumsService;
+  let mongodb: MongoMemoryServer;
 
   beforeEach(async () => {
+    mongodb = await MongoMemoryServer.create();
+    const uri = mongodb.getUri();
+
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        MongooseModule.forRoot(uri),
+        MongooseModule.forFeature([{ name: Album.name, schema: AlbumSchema }]),
+      ],
       providers: [AlbumsService],
     }).compile();
 
--- src/albums/albums.service.ts
+++ src/albums/albums.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import { Album, AlbumDocument } from '../common/schemas/album.model';
 
 @Injectable()
-export class AlbumsService {}
+export class AlbumsService {
+  constructor(
+    @InjectModel(Album.name) private albumModel: Model<AlbumDocument>,
+  ) {}
+
+  async createAlbum(createAlbumDto) {
+    return this.albumModel.create(createAlbumDto);
+  }
+
+  async findAlbums(): Promise<Album[]> {
+    const album = await this.albumModel.find().exec();
+
+    if (!album) {
+      throw new HttpException('Album not found.', HttpStatus.NOT_FOUND);
+    }
+
+    return album;
+  }
+}
