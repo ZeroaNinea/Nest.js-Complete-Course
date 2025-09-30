822bb27dd4e50032a0ce8f1bdbfe1e3526a85e6d
Catgirl <zeroaninea@gmail.com>
2025-09-30 18:49:02 +0400

    the Songs schema added into the imports array

--- src/songs/songs.module.ts
+++ src/songs/songs.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { SongsController } from './songs.controller';
 import { SongsService } from './songs.service';
+import { SongSchema, Song } from '../common/schemas/song.model';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Song.name, schema: SongSchema }]),
+  ],
   controllers: [SongsController],
   providers: [SongsService],
 })
