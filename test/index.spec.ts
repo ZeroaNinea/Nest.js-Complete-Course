f5659508d8e2e3112f66c51a241c9bb87532e438
Catgirl <zeroaninea@gmail.com>
2025-10-02 17:02:40 +0400

    the test refactored

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -2,6 +2,11 @@ import { sum } from '../src/index';
 
 describe('sum()', () => {
   it('adds two numbers', () => {
-    expect(sum(2, 3)).toBe(5);
+    const a: number = Math.random();
+    const b: number = Math.random();
+
+    const c: number = a + b;
+
+    expect(sum(a, b)).toBe(c);
   });
 });
