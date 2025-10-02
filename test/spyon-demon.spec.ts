1bc578d2a60dd6ed829b202606dfb7f25933a3ea
Catgirl <zeroaninea@gmail.com>
2025-10-02 19:53:56 +0400

    'should spy on the class method' passed

--- test/spyon-demon.spec.ts
+++ test/spyon-demon.spec.ts
@@ -9,6 +9,10 @@ const songRepository = {
   findOne: () => {},
 };
 
+class ArtistRepository {
+  save(createArtistDto: { name: string }) {}
+}
+
 describe('spyOn Demo', () => {
   it('should spy on the existing object', () => {
     const spy = jest.spyOn(songRepository, 'create');
@@ -17,4 +21,18 @@ describe('spyOn Demo', () => {
     expect(spy).toHaveBeenCalled();
     expect(spy).toHaveBeenCalledWith({ title: 'test' });
   });
+
+  it('should spy on the class method', () => {
+    const artist = new ArtistRepository();
+    const spy = jest
+      .spyOn(artist, 'save')
+      .mockImplementation((createArtistDto) => createArtistDto);
+
+    artist.save({ name: 'test' });
+
+    expect(spy).toHaveBeenCalled();
+    expect(spy).toHaveBeenCalledWith({ name: 'test' });
+  });
+
+  afterEach(() => jest.resetAllMocks());
 });
