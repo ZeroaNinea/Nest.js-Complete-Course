ac492106dbf010e333fc2c3464f569b2a7045b08
Catgirl <zeroaninea@gmail.com>
2025-10-03 21:19:43 +0400

    log connection notification

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -1,3 +1,4 @@
+import { OnModuleInit } from '@nestjs/common';
 import {
   MessageBody,
   SubscribeMessage,
@@ -11,9 +12,16 @@ import { Server } from 'socket.io';
     origin: '*',
   },
 })
-export class EventsGateway {
+export class EventsGateway implements OnModuleInit {
   @WebSocketServer()
   server: Server;
+
+  onModuleInit() {
+    this.server.on('connection', (socket) => {
+      console.log('Client connected', socket.id);
+      console.log(socket.connected);
+    });
+  }
 
   @SubscribeMessage('message')
   handleMessage(
