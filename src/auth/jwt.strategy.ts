9ed06633e9378c4bda1c10672874b90f70fd4730
Catgirl <zeroaninea@gmail.com>
2025-10-04 15:05:52 +0400

    tests fixed

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -8,12 +8,13 @@ import { PayloadType } from '../common/interface/payload-type.interface';
 // import { authConstants } from './auth.constants';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor(configService: ConfigService) {
+    console.log('JWT_SECRET', configService.get<string>('JWT_SECRET'));
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: configService.get<string>('JWT_SECRET') || '',
+      secretOrKey: configService.get<string>('JWT_SECRET') || 'default_secret',
       // secretOrKey: authConstants.secret,
     });
   }
