ee9103a277abf77e5bc59fcd7935e6caa04eadf8
Catgirl <zeroaninea@gmail.com>
2025-10-04 14:23:04 +0400

    the getSongs method created

--- src/songs/songs.resolver.ts
+++ src/songs/songs.resolver.ts
@@ -1,8 +1,12 @@
-import { Resolver } from '@nestjs/graphql';
-
+import { Resolver, Query } from '@nestjs/graphql';
 import { SongsService } from './songs.service';
 
 @Resolver()
 export class SongsResolver {
   constructor(private songsService: SongsService) {}
+
+  @Query('songs')
+  async getSongs() {
+    return this.songsService.findAll();
+  }
 }
