5c7559f9b3bf6187656020fb9c8ce988be9df315
Catgirl <zeroaninea@gmail.com>
2025-10-04 23:55:20 +0400

    the signupUser method created

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,7 +1,11 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
 
 import { UserService } from '../user/user.service';
 import { AuthService } from './auth.service';
+
+import { CreateUserDto } from '../user/dto/create-user.dto';
+
+import { SignupInput, SignupResponse } from '../graphql';
 
 @Resolver()
 export class AuthResolver {
@@ -9,4 +13,15 @@ export class AuthResolver {
     private userService: UserService,
     private authService: AuthService,
   ) {}
+
+  @Mutation('signup')
+  async signupUser(
+    @Args('signupInput') signupInput: SignupInput,
+  ): Promise<SignupResponse> {
+    const user = await this.userService.create(signupInput as CreateUserDto);
+
+    return {
+      email: user.email,
+    };
+  }
 }
