789c69aca352c53aa4ff74e9e442003ebe296efb
Catgirl <zeroaninea@gmail.com>
2025-10-04 16:09:08 +0400

    the createSong Mutation added

--- src/songs/songs.resolver.ts
+++ src/songs/songs.resolver.ts
@@ -1,17 +1,26 @@
-import { Resolver, Query, Args } from '@nestjs/graphql';
+import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
+
 import { SongsService } from './songs.service';
+import { CreateSongDto } from './dto/create-song.dto';
+
+import { Song } from '../common/entities/song.entity';
 
 @Resolver()
 export class SongsResolver {
   constructor(private songsService: SongsService) {}
 
   @Query('songs')
-  async getSongs() {
+  async getSongs(): Promise<Song[]> {
     return this.songsService.findAll();
   }
 
   @Query('song')
-  async getSong(@Args('id') id: number) {
+  async getSong(@Args('id') id: number): Promise<Song> {
     return this.songsService.findOne(id);
+  }
+
+  @Mutation('createSong')
+  async createSong(@Args('song') song: CreateSongDto): Promise<Song> {
+    return this.songsService.create(song);
   }
 }
