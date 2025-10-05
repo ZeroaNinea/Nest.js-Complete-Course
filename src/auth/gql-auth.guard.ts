166e38239b9e4cc219a9538ec47f308b7977369e
Catgirl <zeroaninea@gmail.com>
2025-10-05 14:59:21 +0400

    canActive and handleRequest methods created

--- src/auth/gql-auth.guard.ts
+++ src/auth/gql-auth.guard.ts
@@ -1,5 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { ExecutionContextHost } from '@nestjs/core/helpers/execution-context-host';
 import { AuthGuard } from '@nestjs/passport';
+import { GqlExecutionContext } from '@nestjs/graphql';
+import { Observable } from 'rxjs';
+import { AuthenticationError } from '@nestjs/apollo';
+
+import { Profile } from '../graphql';
 
 @Injectable()
-export class GraphQLAuthGuard extends AuthGuard('jwt') {}
+export class GraphQLAuthGuard extends AuthGuard('jwt') {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const ctx = GqlExecutionContext.create(context);
+    const result: { req: Profile } = ctx.getContext();
+
+    return super.canActivate(new ExecutionContextHost([result.req]));
+  }
+
+  handleRequest<TUser = any>(err: any, user: any): TUser {
+    if (err || !user) {
+      throw new AuthenticationError('GqlAuthGuard error');
+    }
+
+    return user as TUser;
+  }
+}
